b16e3efd44fb415b3f9047e0f0b9b3553fa95430
nicedfx <nicedfx@gmail.com>
2020-12-27 22:26:30 +0300

    Removed excess code

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -10,7 +10,6 @@ import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 import static helpers.AttachmentHelper.*;
 import static org.openqa.selenium.remote.BrowserType.FIREFOX;
-import static org.openqa.selenium.remote.BrowserType.EDGE;
 
 
 public class TestBase {
@@ -32,15 +31,7 @@ public class TestBase {
             else if (System.getProperty("remote.browser.url").equals("selenoid.autotests.cloud")) {
                 Configuration.remote = "https://user1:1234@" + System.getProperty("remote.browser.url") + ":4444/wd/hub/";
             }
-            else {
-                Configuration.remote = "https://" + System.getProperty("remote.browser.url") + ":4444/wd/hub/";
-            }
         }
-//        else {
-//            Configuration.remote = "http://notebook.home:4444/wd/hub/";
-//        }
-
-//        System.out.println("!@#!@#!@#@# SELENOID URI IS: " + Configuration.remote);
         Configuration.startMaximized = true;
     }
 
@@ -49,13 +40,6 @@ public class TestBase {
         attachScreenshot("Last screenshot");
         attachPageSource();
         attachVideo();
-//        attachAsText("Browser console logs", getConsoleLogs());
-//        attachAsText("Browser console logs", "Just a text");
-//        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-//        System.out.println("Printing out the console logs");
-//        System.out.println(Selenide.getWebDriverLogs(BROWSER));
-//        System.out.println(getConsoleLogs());
-//        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
         closeWebDriver();
         }
 
--- src/test/java/tests/ToolsQaPracticeFormTests.java
+++ src/test/java/tests/ToolsQaPracticeFormTests.java
@@ -1,10 +1,8 @@
 package tests;
 
-import com.codeborne.selenide.Configuration;
 import com.github.javafaker.Faker;
 import com.github.javafaker.service.FakeValuesService;
 import com.github.javafaker.service.RandomService;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
